c308f21485f242af5350ac91eb0de11377ac5b66
Andres <andrespinovallejo@gmail.com>
2020-10-11 22:22:50 -0500

    Add login client

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <Login />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Login from './components/Login'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container
+    id='input-usage'
+    fluid
+  >
+    <v-row>
+      <v-col cols='6'>
+        <v-text-field
+          v-model='username'
+          label='Username'
+        ></v-text-field>
+      </v-col>
+      <v-col cols='6'>
+        <v-text-field
+          v-model='password'
+          label='Password'
+        ></v-text-field>
+      </v-col>
+      <v-col cols='12'>
+        <v-btn
+          block
+          elevation='2'
+          v-on:click='onLogin'
+        >Login</v-btn>
+      </v-col>
+      <v-col cols='12'>
+        <span>{{message}}</span>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Login',
+  data: () => ({
+    username: '',
+    password: '',
+    message: ''
+  }),
+  methods: {
+    onLogin () {
+    this.message = ''
+      const url = 'http://localhost:5000/api/v1/login'
+      const body = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, body)
+        .then(() => {
+          this.message = 'Logged in!'
+        })
+        .catch(() => {
+          this.message = "Wrong email or password"
+        })
+    }
+  }
+}
+</script>
+
+<style></style>
