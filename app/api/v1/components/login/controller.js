54253e104e03ccd299dbf066ccd4523ccccb8a03
Andres Moreno <andresmore06@gmail.com>
2020-10-11 22:36:47 -0500

    Added unauthorized status on invalid login

--- app/api/v1/components/login/controller.js
+++ app/api/v1/components/login/controller.js
@@ -5,13 +5,14 @@ const post = async (req, res) => {
   try {
     username = req.body.username;
     password = req.body.password;
-    winston.debug(`Login attempt ${username} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
 
     if (username === "admin" && password === "123456") {
+      winston.debug(`Login attempt succesful for user ${username} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
       return res
         .status(httpStatus.OK)
         .send({ valid: true });
     }
+    winston.debug(`Login attempt failed for user ${username} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
     return res
       .status(httpStatus.UNAUTHORIZED)
       .send({ valid: false });
